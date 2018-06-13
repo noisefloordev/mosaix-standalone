f639ca05975011f9f722b9f1e9e912070d14c725
Noise Floor <noisefloordev@users.noreply.github.com>
2018-06-12 19:39:44 -0500

    Allow specifying the offset and angle with the commandline interface.

--- mosaix-commandline/CommandLine.cpp
+++ mosaix-commandline/CommandLine.cpp
@@ -1,45 +1,91 @@
 #include <stdexcept>
 #include <algorithm>
+#include "getopt.h"
 #include "../mosaix-core/Mosaic.h"
 #include "ImageIO.h"
 using namespace std;
+
+void usage(string name)
+{
+    printf("Usage: %s [-b block-size] [-x x-offset] [-y y-offset] [-a angle] [-n] input.exr output.exr\n", name.c_str());
+}
 
 int main(int argc, char *argv[])
 {
     // Compressing the output file can take a good portion of the overall processing time.
     // Allow disabling it for batch use.
     bool enable_compression = true;
-    if(argc > 1 && !strcmp(argv[1], "-n"))
-    {
-        enable_compression = false;
-        ++argv;
-        --argc;
+
+    Mosaic::Options options;
+    while(1) {
+        int this_option_optind = optind ? optind : 1;
+        int option_index = 0;
+        static struct option long_options[] = {
+            {"block-size",      required_argument, 0,  'b' },
+            {"no-compression",  no_argument,       0,  'n' },
+            {"angle",           required_argument, 0,  'a' },
+            {"offset-x",        required_argument, 0,  'x' },
+            {"offset-y",        required_argument, 0,  'y'},
+            {0,                 0,                 0,  0 }
+        };
+
+        int c = getopt_long(argc, argv, "b:nha:x:y:", long_options, &option_index);
+        if(c == -1)
+            break;
+
+        switch (c) {
+        case 'n':
+            enable_compression = false;
+            break;
+
+        case 'a':
+            options.angle = (float) atof(optarg);
+            break;
+
+        case 'x':
+            options.offset_x = atoi(optarg);
+            break;
+
+        case 'y':
+            options.offset_y = atoi(optarg);
+            break;
+
+        case 'b':
+            options.block_size = atoi(optarg);
+
+            if(options.block_size == 0)
+            {
+                printf("Invalid block size\n");
+                exit(1);
+            }
+
+            break;
+
+        case 'h':
+            usage(argv[0]);
+            exit(1);
+            break;
+
+        default:
+            break;
+        }
     }
 
-    if(argc != 4)
+    if(optind+2 != argc)
     {
-        printf("Usage: %s [-n] input.exr output.exr block_size\n", argv[0]);
+        usage(argv[0]);
         return 1;
     }
 
-    string input_filename = argv[1];
-    string output_filename = argv[2];
+    string input_filename = argv[optind+0];
+    string output_filename = argv[optind+1];
     try {
         // Read the image.
         Image image;
 
-        int block_size = (int) atoi(argv[3]);
-        if(block_size == 0)
-        {
-            printf("Invalid block size\n");
-            exit(1);
-        }
-
         ImageHelpers::ReadImage(image, input_filename);
 
         // Apply the mosaic.
-        Mosaic::Options options;
-        options.block_size = block_size;
         Mosaic::ApplyMosaic(image, options);
         
         // Write the result.
